f57e27fe61ef6eb04124e8391f7139e8432aef0e
Guilherme Neves <guilhermebuenodasneves@gmail.com>
2026-08-10 10:18:10 -0300

    Update sanity.ts

--- nobrim-site/src/lib/sanity.ts
+++ nobrim-site/src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID || 'SEU_PROJECT_ID',
-  dataset: import.meta.env.SANITY_DATASET || 'production',
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID || 'SEU_PROJECT_ID',
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
   useCdn: false, // false para garantir dados frescos no build time (SSG)
   apiVersion: '2024-03-01', 
   token: import.meta.env.SANITY_API_TOKEN,
